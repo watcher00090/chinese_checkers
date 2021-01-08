42760dfd0bb8a1b0adf02d7823a400a347796c85
DMilan <ckq1347@gmail.com>
2021-01-07 17:52:34 -0800

    created struct for game spaces named Square

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,31 @@ extern crate piston_window;
 
 use piston_window::*;
 
+struct Square<'a> {x:f64, y:f64, c:graphics::types::Color, p:Option<i32>, tl:&'a Square<'a>, tr:&'a Square<'a>, lf:&'a Square<'a>, rt:&'a Square<'a>, bl:&'a Square<'a>, br:&'a Square<'a>}
+
 fn main() {
 
     let H: f64 = 100.0; // hexagon side length
     let C_x = 640.0 / 2.0;
     let C_y = 480.0 / 2.0;
     let angles = [0.0, 60.0, 120.0, 180.0, 240.0, 300.0];
+
     let angles_rad : Vec<f64> = angles.into_iter().map(|angle| {
         return angle * std::f64::consts::PI / 180.0;
     }).collect::<Vec<f64>>();
+
     let points : Vec<[f64; 2]> = angles_rad.into_iter().map(|angle| {
         return [C_x + H * angle.cos(), C_y + H * angle.sin()];
-    }).collect::<Vec<[f64; 2]>>(); 
+    }).collect::<Vec<[f64; 2]>>();
 
     let mut window: PistonWindow = 
         WindowSettings::new("Hello Piston!", [640, 480]).resizable(false).exit_on_esc(true).build().unwrap();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _device| {
             clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      c.transform, g);
+            // rectangle([1.0, 0.0, 0.0, 1.0], // red
+            //           [0.0, 0.0, 100.0, 100.0],
+            //           c.transform, g);
             polygon([0.0,0.0,0.0,1.0], points.as_slice(), c.transform, g);
         });
     }
