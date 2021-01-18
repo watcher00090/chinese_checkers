fb14546460bd71f005b21c2088d90d32ec5e9243
James Pedersen <james00090@gmail.com>
2021-01-17 16:06:18 -0800

    added code to connect board squares to each other

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,64 @@ fn main() {
     // initialize board
     let start : Square = Square{x:1000.0, y:1000.0, c:[0.0, 0.0, 0.0, 0.0], p:None, tl:None, tr:None, lf:None, rt:None, bl:None, br:None};
 
+    let mut prev_row : Vec<&mut Square> = Vec::new();
+    prev_row.push(&start);
+
+    let Nrows : i32 = 17;
+    let j : i32;
+
+    for j in 0..Nrows {
+        let row_num : i32 = j+1;
+
+        let mut cur_row : Vec<&mut Square> = Vec::new();
+        for k in 0..row_length(row_num) {
+            let tmp_square : Square = Square{x:1000.0, y:1000.0, c:[0.0, 0.0, 0.0, 0.0], p:None, tl:None, tr:None, lf:None, rt:None, bl:None, br:None};
+            cur_row.push(&mut tmp_square);
+        }
+
+        for k in 0..row_length(row_num-1) {
+            let mut tmp : &mut Square = *(prev_row.get_mut(k as usize).unwrap());
+            // connect tmp to tile at bottom left
+            if !(row_num == 14 && k == 0)  { // special case for edge elements in top row of bottom triangle
+                if row_length(row_num-1) == 4 { 
+                    tmp.bl = Some(*(cur_row.get_mut(5+k as usize).unwrap()));
+                    let mut top_el : &mut Square = *cur_row.get_mut(5+k as usize).unwrap();
+                    top_el.tr = Some(tmp);
+                } else { 
+                    tmp.bl = Some(*(cur_row.get_mut(k as usize).unwrap())); 
+                    let mut top_el : &mut Square = *cur_row.get_mut(k as usize).unwrap();
+                    top_el.tr = Some(tmp);
+                }
+            }
+            // connecting tmp to tile at bottom right
+            if !(row_num == 14 && k == 3) { // special case for edge elements in top row of bottom triangle
+                if row_length(row_num-1) == 4 { 
+                    tmp.br = Some(*(cur_row.get_mut((5+k+1) as usize).unwrap()));
+                    let mut top_el : &mut Square = *cur_row.get_mut((5+k+1) as usize).unwrap();
+                    top_el.tl = Some(tmp);
+                } else {
+                    tmp.br = Some(*(cur_row.get_mut((k+1) as usize).unwrap()));
+                    let mut top_el : &mut Square = *cur_row.get_mut((k+1) as usize).unwrap();
+                    top_el.tl = Some(tmp);
+                }
+            }
+            if k == 0 {
+                tmp.lf = None;
+            } else {
+                tmp.lf = Some(*(prev_row.get_mut((k-1) as usize).unwrap()));
+            }
+            if k == row_length(row_num-1)-1 {
+                tmp.rt = None;
+            } else {
+                tmp.rt = Some(*(prev_row.get_mut((k+1) as usize).unwrap()));
+            }
+        }
+
+        // another case to handle the yellow at the bottom
+
+
+    }
+
     let mut window: PistonWindow = 
         WindowSettings::new("Hello Piston!", [640, 480]).resizable(false).exit_on_esc(true).build().unwrap();
     while let Some(e) = window.next() {
