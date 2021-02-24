22152f8211e211cd1315de1d82fc0503fbcf1e1a
DMilan <ckq1347@gmail.com>
2021-02-23 17:27:23 -0800

    commiting to merge

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ extern crate piston_window;
 
 use piston_window::*;
 
-struct Square<'a> {x:f64, y:f64, c:graphics::types::Color, p:Option<i32>, tl:&'a Square<'a>, tr:&'a Square<'a>, lf:&'a Square<'a>, rt:&'a Square<'a>, bl:&'a Square<'a>, br:&'a Square<'a>}
+struct Hexpt {x:f64, y:f64, c:graphics::types::Color, p:Option<i32>}
+
+// convert hex coords to x and y
+// get adjacent hex coords
+// define piece struct
+// tell if space is occupied
+// move piece
+
+// create hexboard
+// initialize player positions
+// main loop to run game
 
 // rownum goes from 1 to 17
 fn row_length(rownum:i32) -> i32 {
@@ -31,7 +41,7 @@ fn main() {
     }).collect::<Vec<[f64; 2]>>();
 
     // initialize board
-    let start : Square = Square{x:1000.0, y:1000.0, c:[0.0, 0.0, 0.0, 0.0], p:None};
+    // let start : Square = Square{x:1000.0, y:1000.0, c:[0.0, 0.0, 0.0, 0.0], p:None};
 
     let mut window: PistonWindow = 
         WindowSettings::new("Hello Piston!", [640, 480]).resizable(false).exit_on_esc(true).build().unwrap();
